7dff5b96d3771ac15b4997ec4350e4ce8ea3520c
Andrew Darqui <andrew.darqui@gmail.com>
2014-09-27 22:58:24 -0400

    memcache

--- src/System/DevUtils/Base/Url/Memcache.hs
+++ src/System/DevUtils/Base/Url/Memcache.hs
@@ -1,26 +1,21 @@
 module System.DevUtils.Base.Url.Memcache (
  Memcache(..),
- defaultMemcacheSession,
+ defaultMemcacheConnection,
  defaultMemcache
 ) where
 
 import qualified System.DevUtils.Base.Url.Connection as C
-import qualified System.DevUtils.Base.Url.Session as Ses
 
 data Memcache = Memcache {
- _ses :: Ses.Session,
- _custom :: Maybe String
+ _con :: C.Connection
 } deriving (Show, Read)
 
-defaultMemcacheSession :: Ses.Session
-defaultMemcacheSession = Ses.Session {
- Ses._auth = Nothing,
- Ses._con = C.Connection {
-  C._dest = "localhost", C._port = 11211, C._type = C.TCP }
+defaultMemcacheConnection :: C.Connection
+defaultMemcacheConnection = C.Connection {
+  C._dest = "localhost", C._port = 11211, C._type = C.TCP
  }
 
 defaultMemcache :: Memcache
 defaultMemcache = Memcache {
-  _ses = defaultMemcacheSession,
-  _custom = Nothing
+  _con = defaultMemcacheConnection
  }
